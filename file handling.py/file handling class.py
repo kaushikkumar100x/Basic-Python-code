0b795ed3be67c5a26708cad93f245b790a03bf74
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-04-08 23:37:04 +0530

    third commit

--- file handling.py/file handling class.py
+++ file handling.py/file handling class.py
@@ -1,15 +1,37 @@
-file_name = "note.txt"
+# file_name = "note.txt"
 
-note = input("Enter you note:")
-file = open(file_name, "a")
-file.write(note +"\n")
+# note = input("Enter you note:")
+# file = open(file_name, "a")
+# file.write(note +"\n")
 
-file.close()
+# file.close()
 
 
-print("note add successfully")
+# print("note add successfully \n")
 
-file = open(file_name, "r")
-content = file.read()
-print(content)
-file.close()
+  
+# file = open(file_name, "r")
+# content = file.read()
+# print(content)
+# file.close()
+
+
+
+
+# try and except block
+
+try:
+    num1 = float(input("Enter you first number:"))
+    num2 = float(input("Enter you second number:"))
+    
+    result = num1 / num2
+    print("the result is :", result)
+    
+    
+except ValueError:
+    print("Invalid input . please enter a valid number:")
+except ZeroDivisionError:
+    print("Error: division by zero is not alloweds" )
+finally:
+    print("This program is executed successfully")
+    
